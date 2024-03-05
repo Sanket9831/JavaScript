161de6d7f7c8466e1305783ce381cc46d1cb876d
Sanket Kumar <56168548+Sanket9831@users.noreply.github.com>
2024-03-05 21:57:18 +0000

    Learnt comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(2>1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+// 💡 Best practice is to compare same dat type
+
+
+// console.log(null > 0) // null is converted to 0
+// console.log(null == 0) // null is not converted to 0
+// console.log(null >= 0) // null is converted to 0
+// 💡Equality (==) and COmparision(>, <, >=, <=) works differently for null
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined == 0)
+
+// STRICT CHECK "==="
+// 💡Checks data type as well
+console.log("2" === 2)
+
+
+
+
+
+
