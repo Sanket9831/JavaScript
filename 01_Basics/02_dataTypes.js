bb065fbb1fa3317ebaca5224e88301302fb1eb2c
Sanket Kumar <56168548+Sanket9831@users.noreply.github.com>
2024-03-05 17:37:42 +0000

    Added dataTypes file

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+
+
+let name = "sanket"
+let age = 25
+let isLoggedIn = false
+
+/*PRIMITIVE DATA TYPES*/
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone balue
+//undefined => value not assigned to variable
+//symbol => used for uniqueness
+
+/*typeof can be used to get type*/
+
+ //OBJECTS
+
+ console.log(typeof null) // it is object
+ 
+ console.log(typeof undefined)
