93442bf69096f07254ac4f627bfbbbe046d34ab2
Sanket Kumar <56168548+Sanket9831@users.noreply.github.com>
2024-03-05 18:02:38 +0000

    Learnt conversion and operations

---
+++ 01_Basics/03_conversionAndOpreations.js
@@ -0,0 +1,70 @@
+let score = "30aad"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// null => 0
+// undefined => NaN
+// nvarchar ("33wef", "sadf234fr") => NaN
+//true => 1, false => 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let num = 45
+let StringNumber = String(num)
+// console.log(typeof StringNumber)
+
+//  *************OPREATIONS*****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " sanket"
+let str3 = str1+str2
+// console.log(str3)
+
+//IMPORTANTS
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+// console.log(true)
+// console.log(+true)
+// // console.log(true+) // Gives Error
+// console.log(+ "")
+// console.log(+ "sd")
+
+
+// Not recommended
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let counter = 100
+console.log(counter)
+let counter2 = counter++
+console.log(counter2)
+counter = 100
+let counter3 = ++counter
+console.log(counter3)
